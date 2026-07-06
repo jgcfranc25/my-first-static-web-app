24f9ca80b5837e423631c83a2e196d5e730118dd
Javier <jgcfranc25@gmail.com>
2026-07-06 16:01:05 -0600

    Update website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,23 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return <div>
+  <h1>
+      Responsive Design
+    </h1>
+    <h2>
+      The ViewPort
+    </h2>
+    <article>
+      <section>
+        He escuchado la versión de que hoy en día los libros son más bien cortos por un tema económico. Son más fáciles de producir y de vender. De los libros que tomé el año pasado la mayor parte eran, de hecho, cortos. Pero hubo algunas excepciones. La primera fue Leñador de Mike Wilson (pueden leer mi reseña en 60 Watts aquí). Y, de hecho, tuve que dedicarle varias semanas a la lectura. Porque, para ser sincera, las actividades diarias atentan un poco contra una lectura de largo aliento calmada y concentrada. En mi caso gran parte de las interrupciones vienen desde mi hijo. Acaba de cumplir cuatro años y, aunque le gusta que nos sentemos a leer juntos –cada cual sus libros- él no quiere que lea cien páginas de un tirón.
+
+        Durante el final de año traté de concentrarme en la lectura de La liberación de José Antonio Rivera (la reseña se publicará próximamente). Pero estaba con muchas correcciones de textos, además del resto de la vida. No había publicado en el blog, porque estaba de vacaciones en la playa. Aproveché la oportunidad para llevarme el libro –que tiene unas 600 páginas-, y lo leí en pocos días, concentrada y relajadamente. Increíble lo que significa estar sin preocupaciones de trabajo y estudio por algunos días. Además del tiempo que se va en traslados, aunque yo soy de las que leen en el metro; pero si una maneja, ya es otra cosa.
+
+        A mí me gustan los libros gordos y los flacos. No creo que necesariamente a más número de páginas la calidad sea mejor ni que los textos breves sean simples; me parece que ambas posturas son reduccionistas y que, al final, solo se puede evaluar libro a libro, lectura a lectura. Lo único que me parece es que muchas páginas requieren de más momentos de tranquilidad. Pero el asunto entonces no es la lectura, sino qué ocurre en nuestras vidas que a veces esos momentos escasean. Lo que sí puedo decir es que es muy grato tomar un libro grande e ir pasando sus páginas sin prisas hasta que lo dejo con la tapa hacia abajo porque lo terminé y disfruté. Dicho eso, tengo todavía un par de textos bastante largos que esperan su turno, es de esperar que este sea –ojalá- un año más tranquilo que el pasado.
+      </section>
+    </article>
+    </div>;
 }
 
 export default App;
